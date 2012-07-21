f9dd81282d2de02718348e3266571bb3ed481d9e
memememomo <memememomo@gmail.com>
2012-07-21 15:35:16 +0900

    initial commit

---
+++ lib/WordCounter.pm
@@ -0,0 +1,32 @@
+package WordCounter;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WordCounter -
+
+=head1 SYNOPSIS
+
+  use WordCounter;
+
+=head1 DESCRIPTION
+
+WordCounter is
+
+=head1 AUTHOR
+
+memememomo E<lt>memememomo {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
